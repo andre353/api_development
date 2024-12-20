c99bea7d181836f75765956df3763dc93fa51cc1
andre353 <eva595eva@gmail.com>
2024-12-20 20:00:33 +0300

    add delete method function to test deleting a drink object by id from Postman

--- pythonAPI/application.py
+++ pythonAPI/application.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 app.app_context().push()
+
 
 class Drink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,30 +17,44 @@ class Drink(db.Model):
     def __repr__(self):
         return f"Drink('{self.name}', '{self.description}')"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Hello!"
 
-@app.route('/drinks')
+
+@app.route("/drinks")
 def get_drinks():
     drinks = Drink.query.all()
 
     output = []
 
     for drink in drinks:
-        drink_data = {'name': drink.name, 'description': drink.description}
+        drink_data = {"name": drink.name, "description": drink.description}
         output.append(drink_data)
 
     return {"drinks": output}
 
-@app.route('/drinks/<id>')
+
+@app.route("/drinks/<id>")
 def get_drink(id):
     drink = Drink.query.get_or_404(id)
-    return {'name': drink.name, 'description': drink.description}
+    return {"name": drink.name, "description": drink.description}
 
-@app.route('/drinks', methods=['POST'])
+
+@app.route("/drinks", methods=["POST"])
 def add_drink():
-    drink = Drink(name=request.json['name'], description=request.json['description'])
+    drink = Drink(name=request.json["name"], description=request.json["description"])
     db.session.add(drink)
     db.session.commit()
-    return {'id': drink.id}
+    return {"id": drink.id}
+
+
+@app.route("/drinks/<int:id>", methods=["DELETE"])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "Not found"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"message": "The drink has been deleted"}
