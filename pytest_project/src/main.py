fd655316a7099fdeb899fc105376724bcf4d0587
andre353 <eva595eva@gmail.com>
2025-01-05 19:12:21 +0300

    add if condition to check calculator arguments data types, group calculator class tests into TestCalculator class

--- pytest_project/src/main.py
+++ pytest_project/src/main.py
@@ -3,15 +3,25 @@ from typing import Union
 
 class Calculator:
     def divide(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments must be integers or floats")
+        if y == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         return x / y
 
     def add(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments must be integers or floats")
         return x + y
 
     def subtract(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments must be integers or floats")
         return x - y
 
     def multiply(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments must be integers or floats")    
         return x * y
 
 
--- pytest_project/test_/test_main.py
+++ pytest_project/test_/test_main.py
@@ -1,34 +1,35 @@
 import pytest
 from src.main import Calculator
 
-@pytest.mark.parametrize("x, y, res", [
-    (1, 2, 0.5),
-    (5, -1, -5),
-])
-def test_divide(x, y, res):
-    calculator = Calculator()
-    assert calculator.divide(x, y) == res
+class TestCalculator:
+    @pytest.mark.parametrize("x, y, res", [
+        (1, 2, 0.5),
+        (5, -1, -5),
+    ])
+    def test_divide(self, x, y, res):
+        calculator = Calculator()
+        assert calculator.divide(x, y) == res
 
-@pytest.mark.parametrize("x, y, res", [
-    (1, 2, 3),
-    (5, -1, 4),
-])
-def test_add(x, y, res):
-    calculator = Calculator()
-    assert calculator.add(x, y) == res
+    @pytest.mark.parametrize("x, y, res", [
+        (1, 2, 3),
+        (5, -1, 4),
+    ])
+    def test_add(self, x, y, res):
+        calculator = Calculator()
+        assert calculator.add(x, y) == res
 
-@pytest.mark.parametrize("x, y, res", [
-    (1, 2, -1),
-    (5, -1, 6),
-])
-def test_subtractd(x, y, res):
-    calculator = Calculator()
-    assert calculator.subtract(x, y) == res
+    @pytest.mark.parametrize("x, y, res", [
+        (1, 2, -1),
+        (5, -1, 6),
+    ])
+    def test_subtract(self, x, y, res):
+        calculator = Calculator()
+        assert calculator.subtract(x, y) == res
 
-@pytest.mark.parametrize("x, y, res", [
-    (1, 2, 2),
-    (5, -1, -5),
-])
-def test_multiply(x, y, res):
-    calculator = Calculator()
-    assert calculator.multiply(x, y) == res
+    @pytest.mark.parametrize("x, y, res", [
+        (1, 2, 2),
+        (5, -1, -5),
+    ])
+    def test_multiply(self, x, y, res):
+        calculator = Calculator()
+        assert calculator.multiply(x, y) == res
