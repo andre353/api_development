fe0fdaa5e97ed7760d3aad7a72e22631e3944c04
andre353 <eva595eva@gmail.com>
2025-01-05 18:48:43 +0300

    add parametrized tests

--- pytest_project/src/main.py
+++ pytest_project/src/main.py
@@ -1,2 +1,19 @@
-class A:
-    x = 1
+from typing import Union
+
+
+class Calculator:
+    def divide(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        return x / y
+
+    def add(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        return x + y
+
+    def subtract(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        return x - y
+
+    def multiply(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        return x * y
+
+
+if __name__ == "__main__":
+    calculator = Calculator()
