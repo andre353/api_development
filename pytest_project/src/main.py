48cf2b1c50fd0cefcd1930adcc07583d19b571f0
andre353 <eva595eva@gmail.com>
2025-01-05 19:22:41 +0300

    optimize class Calculator inputs validation into one method

--- pytest_project/src/main.py
+++ pytest_project/src/main.py
@@ -2,26 +2,26 @@ from typing import Union
 
 
 class Calculator:
-    def divide(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+
+    def _validate_input(self, x: Union[int, float], y: Union[int, float]) -> None:
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
             raise TypeError("Both arguments must be integers or floats")
+    def divide(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+        self._validate_input(x, y)
         if y == 0:
             raise ZeroDivisionError("Cannot divide by zero")
         return x / y
 
     def add(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Both arguments must be integers or floats")
+        self._validate_input(x, y)
         return x + y
 
     def subtract(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Both arguments must be integers or floats")
+        self._validate_input(x, y)
         return x - y
 
     def multiply(self, x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Both arguments must be integers or floats")    
+        self._validate_input(x, y)
         return x * y
 
 
