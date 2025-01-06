c5ff8a3e0103a9c310c66c72f0bd16b49d156961
andre353 <eva595eva@gmail.com>
2025-01-06 22:50:37 +0300

    add skip marker

--- pytest/pytest_freecodecamp/test/test_my_functions.py
+++ pytest/pytest_freecodecamp/test/test_my_functions.py
@@ -2,24 +2,35 @@ import time
 import pytest
 import source.my_functions as my_functions
 
+
 def test_add():
     assert my_functions.add(1, 2) == 3
 
-def test_divide():  
-    assert my_functions.divide(1, 2) == 0.5 
+
+def test_divide():
+    assert my_functions.divide(1, 2) == 0.5
+
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
-        my_functions.divide(1, 0)    
+        my_functions.divide(1, 0)
+
 
 def test_add_strings():
     assert my_functions.add("a", "b") == "ab"
 
-# create pytest.ini in root, write folder to run
-# pytest test/test_my_functions.py 
 
+# create pytest.ini in root, write folder to run
+# pytest test/test_my_functions.py
+
+# pytest -m slow
 @pytest.mark.slow
 def test_add_slow():
     time.sleep(5)
+    result = my_functions.add(1, 2)
+    assert result == 3
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add_skip():
     result = my_functions.add(1, 2)
     assert result == 3
