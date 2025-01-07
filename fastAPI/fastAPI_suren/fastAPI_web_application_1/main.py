85ce016142925f5e14021cdab16e2d3407515ffc
andre353 <eva595eva@gmail.com>
2025-01-07 23:12:13 +0300

    initial commit

---
+++ fastAPI/fastAPI_suren/fastAPI_web_application_1/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_index():
+    return {"message": "Hello"}
+
+@app.get("/items/")
+def list_items():
+    return [
+        "Item1",
+        "Item2",
+    ]
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
