0682fa89ead63c7f0634dfeac2e78396ce654e37
andre353 <eva595eva@gmail.com>
2025-01-08 20:02:48 +0300

    add Base class to create db models later

---
+++ fastAPI/fastAPI_suren/fastAPI_web_application_1/core/models/base.py
@@ -0,0 +1,13 @@
+# from typing import List
+# from typing import Optional
+# from sqlalchemy import ForeignKey
+# from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+# from sqlalchemy.orm import Mapped
+# from sqlalchemy.orm import mapped_column
+# from sqlalchemy.orm import relationship
+
+# базовый класс необходим для описания работы моделей базы данных
+class Base(DeclarativeBase):
+    pass
+
