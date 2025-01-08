b346cccc7bc17f3ef8923e6d83663d02d28b365d
andre353 <eva595eva@gmail.com>
2025-01-08 16:14:52 +0300

    add items namespace

---
+++ fastAPI/fastAPI_suren/fastAPI_web_application_1/items_views.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Path
+
+router = APIRouter(prefix="/items", tags=["Items"])
+
+@router.get("/")
+def list_items():
+    return [
+        "Item1",
+        "Item2",
+    ]
+# 1) order is important
+@router.get("/latest/")
+def get_latest_item():
+    return {"items": {"id": "0", "name": "latest"}}
+
+# 2) order is important
+@router.get("/{item_id}/")
+def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
+    return {
+        "item": {
+            "id": item_id,
+        }
+    }
--- fastAPI/fastAPI_suren/fastAPI_web_application_1/main.py
+++ fastAPI/fastAPI_suren/fastAPI_web_application_1/main.py
@@ -1,9 +1,11 @@
-from typing import Annotated
-from fastapi import FastAPI, Path
+from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 import uvicorn
 
+from items_views import router as items_router
+
 app = FastAPI()
+app.include_router(items_router)
 
 # EmailStr - валидатор по почте, есть в pydantic, устанавливается через: pip install "pydantic[email]"
 # EmailStr - сторонняя библиотека, которая позволяет проверять emails
@@ -40,27 +42,6 @@ def add(a: int, b: int):
         "b": b,
         "result": a + b,
     }
-
-@app.get("/items/")
-def list_items():
-    return [
-        "Item1",
-        "Item2",
-    ]
-# 1) order is important
-@app.get("/items/latest/")
-def get_latest_item():
-    return {"items": {"id": "0", "name": "latest"}}
-
-# 2) order is important
-@app.get("/items/{item_id}/")
-def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1_000_000)]):
-    return {
-        "item": {
-            "id": item_id,
-        }
-    }
-
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
