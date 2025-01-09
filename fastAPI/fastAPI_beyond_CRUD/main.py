2bd4df9552952e8adb3255b164219c5b7502b5f5
andre353 <eva595eva@gmail.com>
2025-01-09 17:58:46 +0300

    access headers

--- fastAPI/fastAPI_beyond_CRUD/main.py
+++ fastAPI/fastAPI_beyond_CRUD/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
 from pydantic import BaseModel
 
@@ -34,6 +34,23 @@ class BookCreateModel(BaseModel):
 @app.post("/create_book")
 async def create_book(book_data: BookCreateModel):
     return {"title": book_data.title, "author": book_data.author}
+
+
+@app.get("/get_headers", status_code=201)
+async def get_headers(
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None),
+    host: str = Header(None),
+):
+    request_headers = {}
+
+    request_headers["Accept"] = accept
+    request_headers["Content-Type"] = content_type
+    request_headers["User-Agent"] = user_agent
+    request_headers["Host"] = host
+
+    return request_headers
 
 
 # if __name__ == "__main__":
