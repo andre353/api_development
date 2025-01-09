2be769df826ff11ba30a2ee6bda6d0aefe66e6c8
andre353 <eva595eva@gmail.com>
2025-01-09 14:54:29 +0300

    update gitignore, fastAPI_react_integration/main.py, requirements.txt, python_OOP calculator

--- python_common/python_OOP/calculator.py
+++ python_common/python_OOP/calculator.py
@@ -1,27 +1,26 @@
-from typing import Union
-
-
 class Calculator:
 
-    def _validate_input(self, x: Union[int, float], y: Union[int, float]) -> None:
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Both arguments must be integers or floats")
-
+    def _validate_input(self, x: str, y: str,) -> None:
+        if not x.isdigit() or not y.isdigit():
+            raise TypeError("Оба числа должны быть числами.")
     def _validate_operation(self, operation: str) -> None:
         valid_operations = {"+": "add", "-": "subtract", "*": "multiply", "/": "divide"}
 
         if operation not in valid_operations:
-            raise ValueError("Invalid operation")
+            raise ValueError("Неверно введенная математическая операция.")
 
     def _calculate(
-        self, x: Union[int, float], y: Union[int, float], operation: str
-    ) -> Union[int, float]:
+        self, x: str, y: str, operation: str
+    ) -> float:
         try:
             self._validate_input(x, y)
             self._validate_operation(operation)
+            x = int(x)
+            y = int(y)
+            6
             if operation == "/":
                 if y == 0:
-                    raise ZeroDivisionError("Cannot divide by zero")
+                    raise ZeroDivisionError("На ноль делить нельзя.")
             if operation == "+":
                 return x + y
             elif operation == "-":
@@ -31,23 +30,23 @@ class Calculator:
             elif operation == "/":
                 return x / y
         except (TypeError, ZeroDivisionError, ValueError) as e:
-            print(f"Error: {e}")
+            print(f"Ошибка: {e}")
             return None
 
     def calculate(
-        self, x: Union[int, float], y: Union[int, float], operation: str
-    ) -> Union[int, float]:
+        self, x:int, y: int, operation: str
+    ) -> int:
         return self._calculate(x, y, operation)
 
 
 if __name__ == "__main__":
     calculator = Calculator()
 
-    x = float(input("Enter the first number: "))
-    y = float(input("Enter the second number: "))
-    operation = input("Enter the operation (+, -, *, /): ")
+    x = input("Введите первое число: ")
+    y = input("Введите второе число: ")
+    operation = input("Введите математическую операцию (+, -, *, /): ")
 
     result = calculator.calculate(x, y, operation)
 
     if result is not None:
-        print(f"Result: {result}")
+        print(f"Результат: {result}")
