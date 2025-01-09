fdf6e91e71d18e335415a19149ec2141ad2724e9
andre353 <eva595eva@gmail.com>
2025-01-09 16:29:25 +0300

    get request with url dynamic parameter, name data type str, json as dict return value

--- fastAPI/fastAPI_beyond_CRUD/main.py
+++ fastAPI/fastAPI_beyond_CRUD/main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 async def index():
     return {"messsage":"Hello World"}
 
+@app.get('/greet/{name}')    
+async def greet_name(name: str) -> dict:
+    return {"message": f"Hello {name}"}
+
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", reload=True)
     # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug", debug=True,
